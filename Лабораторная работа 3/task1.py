a5aaf1e4c281be47c81a162e409d77095b29db0f
master-grogu <115426937+master-grogu@users.noreply.github.com>
2023-01-28 12:31:33 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,67 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: int) -> None:
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Страниц {self.pages}."
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float) -> None:
+        if not isinstance(new_duration, float):
+            raise TypeError("Продолжительность аудиокниги должна быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Продолжительность аудиокниги должна быть положительным числом")
+        self._duration = new_duration
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}."
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
